01fdddab378296fa65f36576be5bb456bca584a0
schraderjp <70864583+schraderjp@users.noreply.github.com>
2021-04-04 17:59:41 -0400

    Update and rename config.example.js to config.js

--- config/config.js
+++ config/config.js
@@ -72,7 +72,7 @@ module.exports = {
  *
  *  CUSTOMIZE AND UNCOMMENT THIS FOR PRODUCTION INSTALLATIONS.
  */
-    // httpSafeOrigin: "https://some-other-domain.xyz",
+    httpSafeOrigin: "https://js-cryptpad.heroku.com",
 
 /*  httpAddress specifies the address on which the nodejs server
  *  should be accessible. By default it will listen on 127.0.0.1
